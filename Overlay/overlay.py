71e258e41788f382c51207a981115ea3c5ef8c57
WCG847 <heelmods@gmail.com>
2025-08-09 15:30:44 +0100

    appropriate filename

--- Overlay/overlay.py
+++ Overlay/overlay.py
@@ -6,10 +6,10 @@ class Overlay:
 	def __init__(self, file):
 		with open(file, 'rb', buffering=128) as f:
 			header, unk, segment_start, text_size, data_size, bss_size, static_init, static_init_end = unpack('<8I', f.read(4 * 8))
-			filename = unpack('96s', f.read(96))[0].rstrip(b'\x00').decode('latin1')
+			self.filename = unpack('96s', f.read(96))[0].rstrip(b'\x00').decode('latin1')
 			header_str = header.to_bytes(4, 'little').decode('latin-1')
 			if header_str != 'MWo3':
-				raise PermissionError(f'CANT EXECUTE OVERLAY MODULE {filename} ')
+				raise PermissionError(f'CANT EXECUTE OVERLAY MODULE {self.filename} ')
 			self.text = f.read(text_size)
 			self.data = BytesIO(f.read(data_size))
 			self.bss = BytesIO(b'\x00' * bss_size)
@@ -21,7 +21,7 @@ if __name__ == '__main__':
 		print(f'{o.text}, {o.data.getvalue()}, {o.bss.getvalue()}')
 	else:
 		print('No file')
-	with open('YES.BIN', 'wb') as f:
+	with open(o.filename, 'wb') as f:
 		f.write(o.text)
 		f.write(o.data.getvalue())
 		f.write(o.bss.getvalue())
